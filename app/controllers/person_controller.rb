7e0e3e22505e3c29a66f3a2e3ff77c8974b54721
krtkeyan <karthikepsilon@gmail.com>
2016-11-27 09:23:30 +0530

    Update Test

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -3,6 +3,9 @@ class PersonController < ApplicationController
   end
   def new
   end
+  def edit
+	@person=Person.find(params[:id])
+  end
   def create
  	@person=Person.new(params.fetch(:person,{}).permit(:name, :email, :password, :age, :gender, :country, :accept))
 	if @person.save
@@ -15,5 +18,23 @@ class PersonController < ApplicationController
   end
   def show
 	@person=Person.find(params[:id])
+  end
+  def update
+  	@person=Person.find(params[:id])
+	if @person.update_attributes(name: params[:name],name: params[:email],password: params[:password], age: params[:age], gender: params[:gender], country: params[:country])
+		flash[:notice] = "Updated Successfully"
+                render  :action => "show"
+	else
+		Rails.logger.info(@user.errors.messages.inspect)
+		flash[:notice] = "Update Fails"
+                redirect_to  :action => "show"
+	end
+  end
+  def destroy
+  	@person=Person.find(params[:id])
+        @person.destroy
+	flash[:notice]="Deleted Successfully"
+	redirect_to "/"
+        
   end
 end
