fb3e68a8ac4284241c1a3ea212d55bc186657646
krtkeyan <karthikepsilon@gmail.com>
2016-11-27 11:28:35 +0530

    FixGem

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
 	validates :name, presence: true, length: {maximum:20}
 	validates :age, presence: true, :inclusion => 18..100
-	validates :email, presence:true,uniqueness: true
+	validates :email,presence: true,uniqueness: true
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates :country, presence: true
 	#validates_acceptance_of :accept,message:"Please Check Terms and Conditions"
